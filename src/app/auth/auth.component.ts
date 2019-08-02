c1c9ad4fb49efea094d41af589d02ad2e12471bb
unknown <roman.ostapchak@gmail.com>
2019-08-02 20:50:30 +0300

    add login and registration

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+import { Router } from '@angular/router';
+
+@Component({
+    selector:'home-auth',
+    templateUrl:'./auth.component.html'
+})
+export class AuthComponent implements OnInit {
+
+    constructor(private router: Router){}
+
+    ngOnInit(){
+        this.router.navigate(['/login']);
+    }
+
+}
