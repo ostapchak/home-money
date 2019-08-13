32df8d12ee66266d6a26e5c19b1e8a5652659319
Roman <roman.ostapchak@gmail.com>
2019-08-13 23:25:14 +0300

    Add login validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AuthModule } from './auth/auth.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     AuthModule
   ],
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { AuthComponent } from './auth.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { AuthRoutingModule } from './auth-routing.module';
     ],
     imports: [
         CommonModule,
-        AuthRoutingModule
+        AuthRoutingModule,
+        SharedModule
     
     ]
 })
