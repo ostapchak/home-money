9b83a48b863721d06b514ecf1fcd5ffa27170128
Roman <roman.ostapchak@gmail.com>
2019-09-12 21:48:22 +0300

    System module create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AuthModule } from './auth/auth.module';
 import { HttpClientModule } from '@angular/common/http';
+import { SystemModule } from './system/system.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersService } from './shared/sevices/users.service';
 import { AuthService } from './shared/sevices/auth.service';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { AuthService } from './shared/sevices/auth.service';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    AuthModule
+    AuthModule,
+    SystemModule
   ],
   providers: [UsersService, AuthService],
   bootstrap: [AppComponent]
