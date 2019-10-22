08d9c88576248ff30807359f0a43b81fb35ad355
Roman <roman.ostapchak@gmail.com>
2019-10-22 21:26:02 +0300

    Bill page. Data output 2.0

--- src/app/system/bill-page/currency-card/currency-card.component.ts
+++ src/app/system/bill-page/currency-card/currency-card.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'home-currency-card',
   templateUrl: './currency-card.component.html',
   styleUrls: ['./currency-card.component.scss']
 })
-export class CurrencyCardComponent implements OnInit {
+export class CurrencyCardComponent  {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  @Input() currency: any;
+  currencies: string[] = ['USD'];
+  
 
 }
