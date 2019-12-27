2b74f7bbcab4d166ab164e6d05cd7d6e7a38445f
lion4uk <lion4ukx@gmail.com>
2019-12-27 16:47:51 +0200

    Records page. Event adding. P1

---
+++ src/app/system/shared/models/event.model.ts
@@ -0,0 +1,10 @@
+export class HOMEEvent  {
+   constructor(
+     public type: string,
+     public amount: number,
+     public category: number,
+     public date: string,
+     public description: string,
+     public id?: string, 
+   ) {}
+}
