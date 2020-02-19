80295a293fc305dde58cc967deccda7550bc89ce
Roman <roman.ostapchak@gmail.com>
2020-02-19 22:18:39 +0200

    History Page. Filter 1

--- src/app/system/history-page/history-filter/history-filter.component.ts
+++ src/app/system/history-page/history-filter/history-filter.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { Category } from '../../shared/models/category.model';
 
 @Component({
   selector: 'home-history-filter',
   templateUrl: './history-filter.component.html',
   styleUrls: ['./history-filter.component.scss']
 })
-export class HistoryFilterComponent implements OnInit {
+export class HistoryFilterComponent {
 
-  constructor() { }
+  @Output() onFilterCancel = new EventEmitter<any>();
+  @Output() onFilterApply = new EventEmitter<any>();
 
-  ngOnInit() {
+  @Input() categories: Category[] = [];
+
+  selectedPeriod = 'd';
+  selectedTypes = [];
+  selectedCategories = [];
+    
+  timePeriods = [
+    {type: 'd', label: 'День'},
+    {type: 'w', label: 'Тиждень'},
+    {type: 'M', label: 'Місяць'}
+  ];
+
+  types = [
+    {type: 'income', label: 'Дохід'},  
+    {type: 'outcome', label: 'Витрати'}
+  ];
+
+  closeFilter() {
+    this.selectedTypes = [];
+    this.selectedCategories = [];
+    this.selectedPeriod = 'd';
+    this.onFilterCancel.emit();
+  }
+
+  private calculateInputParams(field: string, checked: boolean, value: string) {
+    if (checked) {
+      this[field].indexOf(value) === -1 ? this[field].push(value) : null;
+    } else {
+      this[field] = this[field].filter(i => i !== value);
+    }
+  }
+
+  handleChangeType({checked, value}) {
+    this.calculateInputParams('selectedTypes', checked, value)
+  }
+
+  handleChangeCategory({checked, value}) {
+   this.calculateInputParams('selectedCategories', checked, value)
+  }
+
+  applyFilter() {
+    this.onFilterApply.emit({
+      types: this.selectedTypes,
+      categories: this.selectedCategories,
+      period: this.selectedPeriod
+    });
   }
 
 }
--- src/app/system/history-page/history-page.component.ts
+++ src/app/system/history-page/history-page.component.ts
@@ -25,6 +25,8 @@ export class HistoryPageComponent implements OnInit, OnDestroy {
 
   chartData = [];
 
+  isFilterVisible = false;
+
   ngOnInit() {
     this.s1 = Observable.combineLatest(
       this.categoriesService.getCategories(),
@@ -52,6 +54,23 @@ export class HistoryPageComponent implements OnInit, OnDestroy {
         }, 0)
       })
     });
+  }
+
+  private toggleFilterVisibility(dir: boolean){
+    this.isFilterVisible = dir;
+  }
+
+  openFilter() {
+    this.toggleFilterVisibility(true);
+  }
+
+  onFilterApply(filterData) {
+    console.log(filterData);
+    
+  }
+
+  onFilterCancel() {
+    this.toggleFilterVisibility(false);
   }
 
   ngOnDestroy() {
